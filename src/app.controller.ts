88ddca0461cfd921287fbae3f580ee290b1eaabd
Ace <bajtgrup@gmail.com>
2024-06-17 21:44:04 -0700

    feat: add `webapi/getbanners` api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { QueryCategoryDto } from './query-category.dto';
+import { QuerySharedResourcesDto } from './dtos/query-shared-resources.dto';
+import { sleep } from './utils';
 
 @Controller()
 export class AppController {
@@ -33,11 +35,28 @@ export class AppController {
     return this.appService.getFileContent('presentations.json');
   }
 
+  @Post('webapi/getbanners')
+  getBanners() {
+    return this.appService.getFileContent('banners.json');
+  }
+
   @Post('webapi/GetCategoriesAndResources')
   getCategoriesAndResources(@Body() body: QueryCategoryDto) {
     if (body.categoryid === 16) {
       return this.appService.getFileContent('categoriesandresources-16.json');
     }
     return this.appService.getFileContent('categoriesandresources.json');
+  }
+
+  @Post('webapi/GetUserPresentationViewsAsync')
+  async getUserPresentationViews(@Body() body: QuerySharedResourcesDto) {
+    const pageNumber = body.pageNumber || 0;
+    const pageSize = body.pageSize || 10;
+    const data = this.appService.getFileContent('userpresentationasync.json');
+    const parsed = JSON.parse(data) as any;
+    const data0 = parsed.Data[0];
+    data0.Result = data0.Result.slice(pageNumber * pageSize, pageNumber * pageSize + pageSize);
+    await sleep(2000);
+    return parsed;
   }
 }
